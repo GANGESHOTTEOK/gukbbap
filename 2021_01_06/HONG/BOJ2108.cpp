88f2ac6da44e8cb54854dec095bb1e235f661d30
kick-snare <hyjhyj0901@naver.com>
2021-01-07 00:39:20 +0900

    double?? double???

--- 2021_01_06/HONG/BOJ2108.cpp
+++ 2021_01_06/HONG/BOJ2108.cpp
@@ -1,52 +1,72 @@
-#include <iostream>
-#include <vector>
-#include <cmath>
-using namespace std;
+    #include <iostream>
+    #include <vector>
+    #include <cmath>
+    #include <algorithm>
+    using namespace std;
 
-int N;
-vector<int> nums;
+    int N;
+    vector<int> nums;
 
-void get_average(){
-    float sum = 0;
+    void get_average(){
+        double sum = 0;
 
-    for(int i=0;i<N;i++)
-        sum+=nums[i];
-    
-    cout <<  round(sum / (float)N);
-}
-
-void get_median(){
-    sort(nums.begin(),nums.end());
-    cout << nums[(N-1)/2];
-}
-
-void get_mode(){
-    int max, sum = 0;
-    int mode[8001];
-
-    for(int i=0;i<N;i++)
-        mode[nums[i]+4000]++;
-    
-    for(int i=0;i<=8000;i++)
-        max = max > mode[i] ? max : mode[i];
-
-    cout << max;
-}
-
-int main(){
-    int tmp;
-
-    cin >> N;
-
-    for(int i=0;i<N;i++){
-        cin >> tmp;
-        nums.push_back(tmp);
+        for(int i=0;i<N;i++)
+            sum+=nums[i];
+        
+        cout <<  round(sum / (double)N) << "\n";
     }
 
-    // get_average();
-    // get_median();
-    get_mode();
-    // get_scope();
+    void get_median(){
+        sort(nums.begin(),nums.end());
+        cout << nums[(N-1)/2] << "\n";
+    }
 
-    return 0;
-}
+    void get_mode(){
+        vector<int> mode_rank;
+        int max[2] = {0,};
+        int mode[8001] = {0,};
+
+        for(int i=0;i<N;i++)
+            mode[nums[i]+4000]++;
+        
+        for(int i=0;i<=8000;i++){
+            if(max[0] < mode[i]){
+                max[0] = mode[i];
+                max[1] = i;
+                mode_rank.clear();
+                mode_rank.push_back(i);
+            }
+            else if(max[0] == mode[i]){
+                mode_rank.push_back(i);
+            }
+        }
+        
+        mode_rank.push_back(8001);
+
+        if(mode_rank[1] != 8001)
+            cout << mode_rank[1]-4000 << "\n";
+        else
+            cout << mode_rank[0]-4000 << "\n";
+    }
+
+    void get_scope(){
+        cout << nums[N-1]-nums[0];
+    }
+
+    int main(){
+        int tmp;
+
+        cin >> N;
+
+        for(int i=0;i<N;i++){
+            cin >> tmp;
+            nums.push_back(tmp);
+        }
+
+        get_average();
+        get_median();
+        get_mode();
+        get_scope();
+
+        return 0;
+    }
