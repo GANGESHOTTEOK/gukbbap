8a40cda87493c2a98930c972d38a0f88be46371b
kick-snare <hyjhyj0901@naver.com>
2021-02-06 23:53:41 +0900

    1931 cpp

---
+++ 02_06/HONG/1931.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int N,arr[100000][2];
+    cin >> N;
+    for(int i=0;i<N;i++) cin >> arr[i][0] >> arr[i][1];
+    int cnt = 0,last = 0;
+    for(int i=0;i<N;i++){
+        if(arr[i][2] >= last) {
+            last = 
+        }
+        
+
+
+    }
+    return 0;
+}
